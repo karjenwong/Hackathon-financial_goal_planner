12ec1579ba026157108647895a312dd444da62a2
Karjen Wong <karjenwong@gmail.com>
2020-01-29 15:05:20 -0800

    added form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Custom.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import DatePicker from "react-date-picker";
 
 function App() {
+  const [name, setName] = useState("");
+  const [goal, setGoal] = useState("");
+  const [savings, setSavings] = useState("");
+  const [date, setDate] = useState(new Date());
+  const [slider, setSlider] = useState("1000");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log({
+      name: name,
+      goal: goal,
+      savings: savings,
+      date: date.toString()
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="margin">
+        <Form onSubmit={handleSubmit}>
+          <Form.Group >
+            <Form.Label>Name:</Form.Label>
+            <Form.Control
+              type="text"
+              value={name}
+              onChange={e => setName(e.target.value)}
+            />
+            <Form.Label>Goal ${goal}</Form.Label>
+            <Form.Control
+              type="number"
+              value={goal}
+              onChange={e => setGoal(e.target.value)}
+            />
+            <Form.Label>Current Savings $</Form.Label>
+            <Form.Control
+              type="number"
+              value={savings}
+              onChange={e => setSavings(e.target.value)}
+            />
+            <Form.Label>
+              Date{" "}
+              <DatePicker
+                className="margin"
+                onChange={e => setDate(e)}
+                value={date}
+              />
+            </Form.Label>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Monthly Expenses ${slider}</Form.Label>
+            <Form.Control
+              type="number"
+              onChange={e => {
+                console.log(e.target.value);
+                setSlider(e.target.value);
+              }}
+            />
+            <Form.Control
+              type="range"
+              value={slider}
+              min={0}
+              max={10000}
+              onChange={e => {
+                console.log(e.target.value);
+                setSlider(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </section>
+ 
     </div>
   );
 }
